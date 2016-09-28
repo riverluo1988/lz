34ed9a1163d46691f2c7a76a12927ee49119289c
riverluo <wenjingluo2015@gmail.com>
2016-09-28 16:43:42 +0800

    add shell file

--- dw/dw_dalily_room_flow.sh
+++ dw/dw_dalily_room_flow.sh
@@ -12,7 +12,7 @@ fi
 year=`date -d $y_date +"%Y"`
 month=`date -d $y_date +"%Y-%m"`
 
-hive_script="/data1/workspace/dw/dw_dalily_room_flow.hql"
+hive_script="/data1/workspace/dw/dw_room_online_count.hql"
 
 echo "hive -hiveconf y_date="${y_date}" -hiveconf year="${year}" -hiveconf  month="${month}" -v -f $hive_script"
 hive -hiveconf y_date="${y_date}" -hiveconf year="${year}" -hiveconf  month="${month}" -v -f $hive_script
